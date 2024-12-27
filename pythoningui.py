ee2b36f6b0ef4ce667444a98b83e24456d06cdca
Kovalski Pingvin <golubgolubovic@yandex.com>
2024-12-27 11:42:54 +0100

    Create pythoningui.py

---
+++ pythoningui.py
@@ -0,0 +1,8 @@
+from tkinter import *
+root = Tk()
+root.title("Welcome to ProjectPigeon24")
+root.geometry('350x200')
+root.mainloop()
+lbl = Label(root, text = "This is a python app with a gui! you thinked that was impossimle? it is'nt")
+lbl.grid()
+
